8de765f2b103619a7a793edefcabf1f9a0a727eb
DevasenaInupakutika <devasena.prasad@gmail.com>
2016-06-29 12:46:37 +0100

    Included OpenID scope to accomodate changes in the web observatory for scope

--- wooidc_configure.js
+++ wooidc_configure.js
@@ -9,7 +9,7 @@ Template.configureLoginServiceDialogForWooidc.fields = function () {
         { property: 'domain', label: 'Domain'},
         { property: 'clientId', label: 'Client ID'},
         { property: 'clientSecret', label: 'Client Secret'},
-        //Commenting below properties as theye are pre-defined endpoint routes.
+        //Commenting below properties as they are the pre-defined endpoint routes.
         //{ property: 'authorizationEndpoint', label: 'Authorization Endpoint'},
         //{ property: 'tokenEndpoint', label: 'Token Endpoint'},
         //{ property: 'userinfoEndpoint', label: 'Userinfo Endpoint'}
